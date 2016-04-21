69fa205de4355577a4500e7ef499bc742b36800d
student <student>
2016-04-21 15:30:57 +0400

    added neuaral function class interface

--- Sources/Headers/NeuralFunctions.hpp
+++ Sources/Headers/NeuralFunctions.hpp
@@ -3,23 +3,34 @@
 
 namespace neural {
 	class Function {
+		private:
+			double(*function) (double, bool);
 		public:
 			class Activation {
 				public:
 					static double softMax(double value, bool accumulate);
-
+					static double softStep(double value, bool accumulate);
+					static double softPlus(double value, bool accumulate);
+					static double softSign(double value, bool accumulate);
+					static double binaryStep(double value, bool accumulate);
+					static double tanH(double value, bool accumulate);
+					static double arcTan(double value, bool accumulate);
+					static double indentity(double value, bool accumulate);
+					static double bentIdentity(double value, bool accumulate);
+					static double gaussian(double value, bool accumulate);
 			};
 			class Combination {
 				public:
-					static double Max(double value, bool accumulate);
-					static double Min(double value, bool accumulate);
-					static double MaxQty(double value, bool accumulate);
-					static double MinQty(double value, bool accumulate);
+					static double max(double value, bool accumulate);
+					static double min(double value, bool accumulate);
+					static double mean(double value, bool accumulate);
+					static double maxQty(double value, bool accumulate);
+					static double minQty(double value, bool accumulate);
+					static double hdtv(double value, bool accumulate);
+					static double yuv(double value, bool accumulate);
+					static double sum(double value, bool accumulate);
 			};
-			
 	};
-	
 }
-
 
 #endif
